8b371e926b68f11c7d465968f9da71e70926a84a
RaykoRadev <christmas898@gmail.com>
2026-01-09 14:25:07 +0100

    removing unused import

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,3 @@
 export * as userService from "./studentServices.js";
-export * as animalService from "./animalService.js";
 export * as teacherService from "./teacherServices.js";
 export * as tasksService from "./tasksServices.js";
