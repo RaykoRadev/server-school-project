0222239aa56c9152c00530a49c638704708c5637
RaykoRadev <christmas898@gmail.com>
2026-01-09 15:08:11 +0100

    adding a check message

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.use(routes);
 
 app.use(globalErrorHandler);
 
+app.get("/health", (req, res) => res.json({ status: "ok" }));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
