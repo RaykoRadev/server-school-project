049808e8109a988eab3785d62a2b530339a4ad74
RaykoRadev <christmas898@gmail.com>
2026-01-09 15:05:04 +0100

    another change

--- src/index.js
+++ src/index.js
@@ -25,7 +25,4 @@ app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(
-    PORT,
-    console.log("Server is listening on http://localhost:3000...")
-);
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
