fab142ed5ab5d86d2497db0dcfb791cdacabd0f5
RaykoRadev <christmas898@gmail.com>
2026-01-23 14:36:17 +0100

    middleware to check the subscription

---
+++ src/middlewares/checkSubscription.js
@@ -0,0 +1,25 @@
+import Teacher from "../models/Teacher.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
+
+export default function checkSubscription(req, res, next) {
+    console.log("SUB middleware start");
+
+    const user = req.user;
+    const now = new Date();
+
+    if (user?.role === "teacher") {
+        if (now > new Date(user.subscriptionExpiresAt)) {
+            throw new Error("subExpired");
+            // return res.status(403).json({
+            //     message: "subExpired",
+            // });
+        }
+    }
+
+    next();
+}
+
+// async function findTeacher(id) {
+//     const teacher = await Teacher.findById(id);
+//     return teacher;
+// }
